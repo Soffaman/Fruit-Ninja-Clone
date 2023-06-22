ca576beba8b9a80c7b66deed561f71095e95910e
Dmytro <112495732+Soffaman@users.noreply.github.com>
2023-06-22 14:55:50 +0300

    Extended the game, and prepare code for build

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    public float minVelo = 0.1f;
+
+    private Collider2D col;
     private Rigidbody2D rb;
+    private Vector3 lastMousePos;
+    private Vector3 mouseVelo;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        col.enabled = isMouseMoving();
         SetBladeToMouse();
     }
 
@@ -22,5 +30,21 @@ public class Blade : MonoBehaviour
         mousePos.z = 10; // distance of 10 units from the camera
 
         rb.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    public bool isMouseMoving()
+    {
+        Vector3 curMousePos = transform.position;
+        float traveled = (lastMousePos - curMousePos).magnitude;
+        lastMousePos = curMousePos;
+
+        if(traveled > minVelo)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
